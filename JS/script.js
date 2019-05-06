c887beeda6736582f5cd8b654a7ec68c8788a923
Hieu Xuan Thuy Tran <tranhieuxuanthuy@gmail.com>
2019-05-06 10:54:40 +0700

    Update script.js
    1. Add specific comments.
    2. Shorten some code.
    3. Change the name of attribute to easy understand.
    4. Add 'use strict'

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,21 @@
+'use strict';
+
 const studentList = document.querySelector('.student-list');
 const eachStudent = studentList.children;
 const pagination = document.querySelector('.pagination');
 const buttonList = pagination.querySelector('ul');
-const studentsPerPage = 10;
+const studentsPerPage = 5;
 const searchStudent = document.querySelector('.student-search');
 const noResult = document.querySelector('.no-result');
 
+// Total pages to show all students in list (depends on number students per page )
 function numberOfPages() {
     let totalPages = Math.ceil(eachStudent.length / studentsPerPage);
     return totalPages;
 }
 
+
+// Add a loop to create specific number page (depends on total pages )
 for (let i = 1; i <= numberOfPages(); i++) {
     let pageli = document.createElement('li');
     let pageLink = document.createElement('a');
@@ -21,7 +26,8 @@ for (let i = 1; i <= numberOfPages(); i++) {
     pageli.appendChild(pageLink);
 }
 
-function tenStudentsFirstPage() {
+// Show just some students on first page when the page loads (depends on 'studentsPerPage' value)
+function studentsFirstPage() {
   for (let i = 0; i < eachStudent.length; i++) {
     if ( i < studentsPerPage ) {
       eachStudent[i].style.display = '';
@@ -30,8 +36,9 @@ function tenStudentsFirstPage() {
     }
   }
 }
-tenStudentsFirstPage();
+studentsFirstPage();
 
+//Create a input box and a button to search for students
 const inputBox = document.createElement('input');
 const searchButton = document.createElement('button');
 function searchBox() {
@@ -42,31 +49,31 @@ function searchBox() {
 }
 searchBox();
 
+//Show results corresponding to input value
 const searchResults = [];
 searchButton.addEventListener('click', () => {
-    const filter = inputBox.value.toUpperCase();
-    searchResults.length = 0;
-    for (let i = 0; i < eachStudent.length; i++) {
-        if (eachStudent[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            eachStudent[i].style.display = '';        
+    const filter = inputBox.value.toUpperCase(); //giá trị nhập vào
+    searchResults.length = 0;                    //Mảng trống
+    for (let i = 0; i < eachStudent.length; i++) {//Lặp qua tất cả HS
+        if (eachStudent[i].innerHTML.toUpperCase().indexOf(filter) > -1) {// Đưa vị trí của giá trị nhập vào trong eachStudent để liểm tra nó có tồn tại k
+            eachStudent[i].style.display = '';   // Nếu có tồn tại nó sẽ xuất ra giá trị     
         } else {
-            eachStudent[i].style.display = 'none';
+            eachStudent[i].style.display = 'none'; //Nếu không tồn tại nó sẽ chạy tiếp vòng lặp đến khi nào giá trị nhập tồn tại trong eachS
             searchResults.push(i);
         }   
     }
-    if (searchResults.length === eachStudent.length) {
+    if (searchResults.length === eachStudent.length) { 
         noResult.innerHTML = '<br><hr><br><h1>No Results</h1>';
     } else {
         noResult.innerHTML = ''; 
     }
 });  
 
-
-pagination.addEventListener('click', (event) => {
-  noResult.innerHTML = ''; 
-  let buttonNumber = parseInt(event.target.textContent);
-  let max = buttonNumber * 10; 
-  let min = max - 10;
+// Add function of each button page when 'click' to show different students per page
+buttonList.addEventListener('click', (eachPage) => {
+  let buttonEachPage = parseInt(eachPage.target.textContent);
+  let max = buttonEachPage * studentsPerPage; 
+  let min = max - studentsPerPage;
   for (let i = 0; i < eachStudent.length; i++) {
       if (i >= min && i < max) {
           eachStudent[i].style.display = '';
